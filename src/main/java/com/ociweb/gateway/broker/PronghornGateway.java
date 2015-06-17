11c9164364a149f1b4e775b933417af1085a333d
Nathan Tippy <nathan@tippy.name>
2015-06-17 14:13:27 -0500

    Added placeholder class to enable build to continue

---
+++ src/main/java/com/ociweb/gateway/broker/PronghornGateway.java
@@ -0,0 +1,10 @@
+package com.ociweb.gateway.broker;
+
+public class PronghornGateway {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Placeholder until this class is finished");
+	}
+
+}
